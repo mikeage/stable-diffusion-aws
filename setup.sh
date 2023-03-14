fc8919c1aa6e5c916f37d85b348d1b0f256b3062
Mike Miller <github@mikeage.net>
2023-03-14 05:23:17 +0200

    Clean up setup.sh

--- setup.sh
+++ setup.sh
@@ -6,18 +6,18 @@ set -e
 sudo sed -i "/#\$nrconf{restart} = 'i';/s/.*/\$nrconf{restart} = 'a';/" /etc/needrestart/needrestart.conf
 sudo apt update
 # sudo apt upgrade -y
-sudo apt install wget git python3 python3-venv build-essential net-tools -y
+sudo apt install git python3-venv build-essential net-tools -y
 
 # install CUDA (from https://developer.nvidia.com/cuda-downloads)
 cd /tmp
-sudo -u ubuntu wget https://developer.download.nvidia.com/compute/cuda/12.0.0/local_installers/cuda_12.0.0_525.60.13_linux.run
+sudo -u ubuntu wget --no-verbose https://developer.download.nvidia.com/compute/cuda/12.0.0/local_installers/cuda_12.0.0_525.60.13_linux.run
 sudo sh cuda_12.0.0_525.60.13_linux.run --silent
 sudo -u ubuntu rm cuda_12.0.0_525.60.13_linux.run
 
 cd /home/ubuntu
-sudo -u ubuntu git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git 
-sudo -u ubuntu wget https://huggingface.co/stabilityai/stable-diffusion-2-1-base/resolve/main/v2-1_512-ema-pruned.ckpt -P stable-diffusion-webui/models/Stable-diffusion/
-sudo -u ubuntu wget https://raw.githubusercontent.com/Stability-AI/stablediffusion/main/configs/stable-diffusion/v2-inference.yaml -P stable-diffusion-webui/models/Stable-diffusion/ -O v2-1_512-ema-pruned.yaml 
+sudo -u ubuntu git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git
+sudo -u ubuntu wget --no-verbose https://huggingface.co/stabilityai/stable-diffusion-2-1-base/resolve/main/v2-1_512-ema-pruned.ckpt -P stable-diffusion-webui/models/Stable-diffusion/
+sudo -u ubuntu wget --no-verbose https://raw.githubusercontent.com/Stability-AI/stablediffusion/main/configs/stable-diffusion/v2-inference.yaml -P stable-diffusion-webui/models/Stable-diffusion/ -O v2-1_512-ema-pruned.yaml
 
 cat <<EOF | sudo tee /usr/lib/systemd/system/sdwebgui.service
 [Unit]
